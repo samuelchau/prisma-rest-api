f5c67797da77da09529ef4b529bb31058a948fc5
Samuel Chau <samuel.chau@mail.mcgill.ca>
2022-06-18 22:21:58 +0000

    Added two more API endpoints

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
 // #2 You instantiate PrismaClient by calling the constructor and obtain an instance called prisma.
 // #3 You create your Express app by calling express().
 // #4 You add the express.json() middleware to ensure JSON data can be processed properly by Express.
-// #5 You implement your first route by adding the api endpoint between the calls to app.use and app.listen.
+// #5 You implement your routes by adding the api endpoints between the calls to app.use and app.listen.
 // #6 You start the server on port 3000.
 
 // #1
@@ -20,7 +20,32 @@ const app = express()
 // #4
 app.use(express.json())
 
-// #5
+
+// #5.1 Fetches all released songs.
+app.get('/playlist', async (req, res) => {
+    const songs = await prisma.song.findMany({
+        where: { released: true },
+        include: { singer: true }
+    })
+    res.json({
+        success: true,
+        payload: songs,
+    })
+})
+
+// #5.2 Fetches a specific song by its ID.
+app.get(`/song/:id`, async (req, res) => {
+    const { id } = req.params
+    const song = await prisma.song.findFirst({
+        where: { id: Number(id) },
+    })
+    res.json({
+        success: true,
+        payload: song,
+    })
+})
+
+// #5.3 Fetches all artists.
 app.get('/artists', async (req, res) => {
   const artists = await prisma.artist.findMany()
   res.json({
